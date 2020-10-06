6a369f911b553adc1e8dc13611ac93dcfa2b1f36
Salo <salomonco@gmail.com>
2020-10-06 19:17:28 -0400

    it wroks, adding stretchies

---
+++ script.js
@@ -0,0 +1,71 @@
+// The headline element
+const display = document.querySelector('#display')
+// The input field for size
+const inputSize = document.querySelector('#input-size')
+// The element that displays the value 
+const showSize = document.querySelector('#show-size')
+// Input - Enter Text
+const enterText = document.querySelector('#enter-text')
+// Input Color
+const inputColor = document.querySelector('#input-color')
+// Input bg Color
+const bgColor = document.querySelector('#input-bg-color')
+// Chose Font 
+const font = document.querySelector('#font-selector')
+// Affect bg Color
+const headDisplay = document.querySelector('#headline-display')
+
+//show font
+const showFont = document.querySelector('#show-font')
+// show text color
+const showColor = document.querySelector('#show-color')
+// show back color
+const showBgColor = document.querySelector('#show-bg-color')
+
+inputSize.addEventListener('input', handleInput)
+showSize.addEventListener('input', handleInput)
+enterText.addEventListener('input', handleInput)
+inputColor.addEventListener('input', handleInput)
+bgColor.addEventListener('input', handleInput)
+font.addEventListener('input', handleInput)
+
+function handleInput() {
+    // get the font size:
+    const fontSize = inputSize.value + 'px'
+    // Set the style 
+    display.style.fontSize = fontSize
+    // Show the size
+    showSize.innerHTML = fontSize
+    
+        
+    // Get Font Color
+    const fColor = inputColor.value
+    // Set Color
+    display.style.color = fColor
+    //Show Color --> NEEDS WIP
+    showColor.innerHTML = fColor
+
+    //Get font Family
+    const fontFam = font.value
+    // Set font Family - NEEDS IMPROVEMENT
+    display.style.fontFamily = fontFam
+    // Show Family
+    showFont.innerHTML = fontFam
+    
+    // Get background Color
+    const backColor = bgColor.value
+    // Set Bg Color
+    headDisplay.style.backgroundColor = backColor
+    
+    // Show bg color - WIP
+    showBgColor.innerHTML = backColor
+
+    // Get Text 
+    const getText = enterText.value
+    // Set Text
+    display.innerHTML = getText
+    
+
+}
+
+ 
